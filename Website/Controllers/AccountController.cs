8ea416037f2b1746d70404f40bc17f574e13b6eb
Max Wang <mwang@1080agile.com>
2018-02-08 15:36:35 +1100

    add recaptcha

--- Website/Controllers/AccountController.cs
+++ Website/Controllers/AccountController.cs
@@ -371,7 +371,7 @@ namespace Website.Controllers
             if (ModelState.IsValid)
             {
                 var user = await _userManager.FindByEmailAsync(model.Email);
-                if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+                if (user == null || user.LastLoginDate.HasValue == false || !(await _userManager.IsEmailConfirmedAsync(user)))
                 {
                     // Don't reveal that the user does not exist or is not confirmed
                     return RedirectToAction(nameof(ForgotPasswordConfirmation));
